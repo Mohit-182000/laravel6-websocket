74317c32e6bdcf1e16ababa2ba247c411c2560fa
vishal keraliya <vishalkeraliya009@gmail.com>
2022-05-26 08:16:50 +0530

    initial commit

---
+++ app/Console/Commands/TestCron.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Events\TestEvent;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Auth;
+
+class TestCron extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'test:cron';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        broadcast(new TestEvent(true));
+    }
+}
